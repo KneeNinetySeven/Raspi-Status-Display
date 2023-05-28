dbe7da823b47ec10f446738ce58bd0835336e1c9
KneeNinetySeven <nils.kneemeyer@outlook.com>
2023-05-28 23:09:36 +0200

    New formatter does the thing

--- display.py
+++ display.py
@@ -3,9 +3,6 @@ import logging.handlers
 import math
 import time
 from time import sleep
-import board
-import busio
-import adafruit_ssd1306
 from PIL import Image, ImageDraw, ImageFont, ImageChops
 from pages.disk_page import DiskPage
 from pages.info_page import InfoPage
@@ -21,7 +18,7 @@ WIDTH = 128
 HEIGHT = 64
 BORDER = 5
 
-REFRESH_RATE = .5
+REFRESH_RATE = 0.5
 MILLIS_PER_PAGE = 20000
 
 activePages = [
@@ -35,32 +32,45 @@ font = ImageFont.truetype("Ubuntu-Bold.ttf", size=12)
 
 OLED = None
 
+
 def run(mocked=True):
     IS_MOCKED = mocked
     logger = logging.getLogger()
-    logger.info('Starting monitor')
+    logger.info("Starting monitor")
 
     if not mocked:
+        import board
+        import busio
+        import adafruit_ssd1306
+
         i2c = busio.I2C(board.SCL, board.SDA)
         OLED = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
     else:
         logger.info(
-            'Status display is running mocked up and will only display output.bmp')
+            "Status display is running mocked up and will only display output.bmp"
+        )
     # loadingScreen = LoadingScreen(OLED)
-#    loadingScreen.dispose()
+    #    loadingScreen.dispose()
     running = True
 
     cycleTime = get_current_time_millis()
     pageIndex = 0
     try:
+        image = Image.new('1', (WIDTH, HEIGHT))
         while running:
             currentPage = activePages[pageIndex]
 
             # Display image
-            logger.debug('Loading image')
+            logger.debug("Loading image")
             image = currentPage.getImage()
-            image = drawFooter(currentPage, pageIndex, len(
-                activePages), get_current_time_millis() - cycleTime, MILLIS_PER_PAGE, image)
+            image = drawFooter(
+                currentPage,
+                pageIndex,
+                len(activePages),
+                get_current_time_millis() - cycleTime,
+                MILLIS_PER_PAGE,
+                image,
+            )
 
             if mocked:
                 image.save("output.bmp")
@@ -68,19 +78,23 @@ def run(mocked=True):
                 OLED.image(image)
                 OLED.show()
 
-            logger.debug('Awaiting next frame...')
+            logger.debug("Awaiting next frame...")
 
-            if (get_current_time_millis() - cycleTime >= MILLIS_PER_PAGE):
-                logger.info('Millis passed %s // NextPage: %s' %
-                            (get_current_time_millis() - cycleTime, activePages[pageIndex].getName()))
-                pageIndex = (pageIndex + 1) if (pageIndex +
-                                                1) < len(activePages) else 0
+            if get_current_time_millis() - cycleTime >= MILLIS_PER_PAGE:
+                logger.info(
+                    "Millis passed %s // NextPage: %s"
+                    % (
+                        get_current_time_millis() - cycleTime,
+                        activePages[pageIndex].getName(),
+                    )
+                )
+                pageIndex = (pageIndex + 1) if (pageIndex + 1) < len(activePages) else 0
                 cycleTime = get_current_time_millis()
 
-            sleep(.5)
+            sleep(0.5)
 
     except InterruptedError:
-        logger.info('OK guys. We\'ve been interrupted!')
+        logger.info("OK guys. We've been interrupted!")
         currentPage.finished = True
         running = False
         sendToSleep()
@@ -90,59 +104,89 @@ def get_current_time_millis():
     return round(time.time() * 1000)
 
 
-def drawFooter(page: Page, index, activePages, currentMillis, targetMillis, image: Image) -> Image:
+def drawFooter(
+    page: Page, index, activePages, currentMillis, targetMillis, image: Image
+) -> Image:
     draw = ImageDraw.Draw(image)
     strokeWidth = 1
     footerOffset = 16
     footerTextOffsetLeft = 3
-    draw.line([(0, image.height - footerOffset),
-               (image.width, image.height - footerOffset)],
-              'white', width=strokeWidth,)
-    draw.line([(0, image.height - footerOffset),
-               (image.width * (currentMillis / targetMillis), image.height - footerOffset)],
-              'white', width=strokeWidth*3,)
-    draw.line([(math.floor(image.width/2), image.height - footerOffset),
-               (math.floor(image.width/2), image.height)],
-              'white', width=strokeWidth,)
+    draw.line(
+        [(0, image.height - footerOffset), (image.width, image.height - footerOffset)],
+        "white",
+        width=strokeWidth,
+    )
+    draw.line(
+        [
+            (0, image.height - footerOffset),
+            (image.width * (currentMillis / targetMillis), image.height - footerOffset),
+        ],
+        "white",
+        width=strokeWidth * 3,
+    )
+    draw.line(
+        [
+            (math.floor(image.width / 2), image.height - footerOffset),
+            (math.floor(image.width / 2), image.height),
+        ],
+        "white",
+        width=strokeWidth,
+    )
 
-    draw.text((0 + footerTextOffsetLeft, image.height - draw.textsize(page.getName())
-              [1] - footerTextOffsetLeft), page.getName(), 'white', font=font)
+    draw.text(
+        (
+            0 + footerTextOffsetLeft,
+            image.height - draw.textsize(page.getName())[1] - footerTextOffsetLeft,
+        ),
+        page.getName(),
+        "white",
+        font=font,
+    )
 
-    dotOffsetX = (image.width / 2)
-    dotSpacingX = (image.width / 2 / activePages)
+    dotOffsetX = image.width / 2
+    dotSpacingX = image.width / 2 / activePages
 
     dotSize = min([footerOffset - 8, dotSpacingX - 6])
 
     for dotNum in range(activePages):
-        xBoundaryLeft = dotOffsetX + \
-            (dotSpacingX / 2) + (dotNum * dotSpacingX) - dotSize/2
-        xBoundaryRight = dotOffsetX + \
-            (dotSpacingX / 2) + (dotNum * dotSpacingX) + dotSize/2
-        bounds = [(xBoundaryLeft,  image.height - (footerOffset/2) - (dotSize / 2)),
-                  (xBoundaryRight, image.height - (footerOffset/2) + (dotSize / 2))]
+        xBoundaryLeft = (
+            dotOffsetX + (dotSpacingX / 2) + (dotNum * dotSpacingX) - dotSize / 2
+        )
+        xBoundaryRight = (
+            dotOffsetX + (dotSpacingX / 2) + (dotNum * dotSpacingX) + dotSize / 2
+        )
+        bounds = [
+            (xBoundaryLeft, image.height - (footerOffset / 2) - (dotSize / 2)),
+            (xBoundaryRight, image.height - (footerOffset / 2) + (dotSize / 2)),
+        ]
 
         if dotNum == index:
-            logging.debug('Drawing %s filled' % dotNum)
-            draw.pieslice(bounds, 0, 359, fill='white')
+            logging.debug("Drawing %s filled" % dotNum)
+            draw.pieslice(bounds, 0, 359, fill="white")
         else:
-            logging.debug('Drawing %s empty' % dotNum)
-            draw.arc(bounds, 0, 359, fill='white', width=1)
+            logging.debug("Drawing %s empty" % dotNum)
+            draw.arc(bounds, 0, 359, fill="white", width=1)
 
     return image
 
 
 def sendToSleep(self, *args):
-    logging.getLogger().info('Going into sleep mode.')
+    logging.getLogger().info("Going into sleep mode.")
 
-    sleepImg = Image.open('img/sleep.png')
-    sleepImg = sleepImg.convert('1')
+    sleepImg = Image.open("img/sleep.png")
+    sleepImg = sleepImg.convert("1")
     sleepImg = sleepImg.resize((64, 64))
     sleepImg = ImageChops.invert(sleepImg)
-    img = Image.new('1', (WIDTH, HEIGHT))
-    img.paste(sleepImg, (int((img.width / 2) - (sleepImg.width / 2)),
-              int((img.height / 2) - (sleepImg.height/2))))
+    img = Image.new("1", (WIDTH, HEIGHT))
+    img.paste(
+        sleepImg,
+        (
+            int((img.width / 2) - (sleepImg.width / 2)),
+            int((img.height / 2) - (sleepImg.height / 2)),
+        ),
+    )
     if IS_MOCKED:
-        img.save('output.bmp')
+        img.save("output.bmp")
     else:
         OLED.image(img)
         OLED.show()
