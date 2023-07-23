e01776bfcec7b1e73a696e0e599140cfdb8b1298
KneeNinetySeven <nils.kneemeyer@outlook.com>
2023-07-23 18:49:12 +0200

    Only restarting system service, if running

--- script/update.sh
+++ script/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+systemctl is-active --quiet display
+serviceStatus=$?
+
 echo ++     Pulling changes from GitHub
-git checkout . #To avoid any git issues, no local changes should be made when using the update script.
+#git checkout . #To avoid any git issues, no local changes should be made when using the update script.
 git pull
 echo
 
@@ -9,9 +12,18 @@ echo ++     Installing python dependencies
 sudo -H pip3 install -r requirements.txt
 echo
 
-echo ++     reload system daemons and restart display
+echo ++     Reload system daemons
 sudo systemctl daemon-reload 
-sudo systemctl restart display
 echo
+
+if [[ serviceStatus -eq 0 ]]
+then
+    echo ++     System service in use. Restarting...
+    sudo systemctl restart display
+    echo 
+else 
+    echo ++     System service inactive. 
+    echo 
+fi
 
 echo "UPDATE FINISHED."
